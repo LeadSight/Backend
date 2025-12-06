e4ad19317ce7b62fb1a6329e58672c2e7a332bf8
Yywtv <geenglebellyon@gmail.com>
2025-12-06 22:32:10 +0700

    minor prompt bug fixes

--- src/controllers/promptControllers.js
+++ src/controllers/promptControllers.js
@@ -1,16 +1,16 @@
 const response = require('../helpers/response');
-const generateContentModel = require('../models/insightModel');
+const { generateContentModel } = require('../models/insightModel');
 const PromtValidator = require('../validator/prompt');
 
 
 const promptController = {
     generateInsight: async (req, res) => {
-        try{
+        try{            
             PromtValidator.validatePromptPayload(req.body);
 
-            const result = await generateContentModel(req.body)
+            const text = await generateContentModel(req.body)
 
-            return response(res, 'Generate Content Successfully', 200, 'success', { noteId });
+            return response(res, 'Generate Content Successfully', 200, 'success', { text });
         }
         catch (err){
             return response(res, 'Failed to GenerateContent', err.statusCode ? err.statusCode : 500, 'fail', { error: err.message });
--- src/validator/prompt/schema.js
+++ src/validator/prompt/schema.js
@@ -3,12 +3,12 @@ const Joi = require("joi")
 const promptPayloadSchema = Joi.object({
     age: Joi.number().positive().required(),
     job: Joi.string().required(),
-    balance: Joi.number().required,
+    balance: Joi.number().required(),
     poutcome: Joi.string().required(),
-    campaign: Joi.number().required,
+    campaign: Joi.number().required(),
     duration: Joi.number().required(),
     cons_price_idx: Joi.number().required(),
-    cons_conf_idf: Joi.number().required(),
+    cons_conf_idx: Joi.number().required(),
 })
 
 module.exports = {
